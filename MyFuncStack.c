75b82918078601148e3ff9b02847b4e9e20d0b7d
zenon-paul <123747386+zenon-paul@users.noreply.github.com>
2023-02-05 18:22:53 +0900

    Update MyFuncStack.c

--- MyFuncStack.c
+++ MyFuncStack.c
@@ -1,24 +1,22 @@
 #include<MyStack.h>
-int push(int y,d_stack* x){
-    if((x->tail+1) > (x->leng-1))
-    {
-	return 0;
-    }  
-    x->body[x->tail+1]=y;
-    x->tail++;
-    return 1;
+int s_push(d_stack* x,int input) {
+	if ((x->tail + 1) > (x->leng - 1)) {
+		return 0;
+	}
+	x->body[x->tail + 1] = input;
+	x->tail++;
+	return 1;
 }
-int pop(int* y,d_stack* x){
-    if(x->tail<=-1)
-    {
-        return 0;
-    }
-    *y = x->body[x->tail];
-    x->tail--;
-    return 1;
+int s_pop(d_stack* x,int* result) {
+	if (x->tail < 0) {
+		return 0;
+	}
+	*result = x->body[x->tail];
+	x->tail--;
+	return 1;
 }
-void is(d_stack *x,int *y,int n){
-    x->tail=-1;
-    x->leng=n;
-    x->body=y;
+void is( d_stack* x, int* b, int l) {
+	x->leng = l;
+	x->tail = -1;
+	x->body = b;
 }
