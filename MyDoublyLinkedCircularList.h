6bb77a879219e2f87a93494eade33dd2b7ad0519
zenon-paul <123747386+zenon-paul@users.noreply.github.com>
2023-03-31 09:38:31 +0900

    Update MyDoublyLinkedCircularList.h

--- MyDoublyLinkedCircularList.h
+++ MyDoublyLinkedCircularList.h
@@ -14,4 +14,9 @@ typedef struct list_data {
     int elm;
     struct list_node* head;
 }d_list;
+void il(d_list* x);
+struct list_node* get_elm_addres(d_list* x, int index);
+int add_substitute(d_list* x, int index, int n1, int n2, int n3, char* s);
+int dlt(d_list* x, int index);
+int disp(d_list* x);
 #endif
